dbe6af4716767ef284cd3fd525f811ff225b8928
Linusp <linusp1024@gmail.com>
2016-08-24 08:23:23 +0800

    update class Corpus

--- epoet/corpus.py
+++ epoet/corpus.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 
-import numpy as np
 from keras.utils import np_utils
+from keras.preprocessing.sequence import pad_sequences
 
 
 class Corpus(object):
@@ -15,7 +15,7 @@ class Corpus(object):
         self.inv_dict_ = {self._EOS_: 0}
 
         self.build_from_corpus(corpus_text)
-        self.max_len = max(len(sent) for sent in corpus_text)
+        self.max_len = max(len(sent) for sent in corpus_text) + 1
 
     def build_from_corpus(self, corpus):
         for sent in corpus:
@@ -32,14 +32,68 @@ class Corpus(object):
 
     def generator(self, batch_size=128):
         dict_size = len(self.dict_)
-        data = np.zeros((batch_size, self.max_len, dict_size))
+        EOS_ONEHOT = np_utils.to_categorical([self.inv_dict_[self._EOS_]], dict_size)
+        data = []
 
         for i, sent in enumerate(self.corpus_):
-            data[i % batch_size, :len(sent)] = np_utils.to_categorical(sent, dict_size)
-
-            if len(sent) < self.max_len:
-                remain_len = self.max_len - len(sent)
-                data[i % batch_size, len(sent):] = np_utils.to_categorical([0] * remain_len, dict_size)
+            data.append(np_utils.to_categorical(sent, dict_size))
 
             if (i + 1) % batch_size == 0:
-                yield data
+                yield pad_sequences(data, self.max_len, value=EOS_ONEHOT)
+                data = []
+
+
+class PairCorpus(object):
+    _EOS_ = '^'
+
+    def __init__(self, pair_corpus):
+        """[(xxx, xxx), (xxx, xxx)]"""
+        self.corpus_ = []
+        self.dict_ = {0: self._EOS_}
+        self.inv_dict_ = {self._EOS_: 0}
+
+        self.build_from_corpus(pair_corpus)
+        self.max_len = max(len(pair[0]) for pair in pair_corpus) + 1
+
+    def build_from_corpus(self, pair_corpus):
+        def vectorize_update_dict(sent):
+            sent_vec = []
+            for ch in sent:
+                if ch not in self.inv_dict_:
+                    cur_index = len(self.inv_dict_)
+                    self.inv_dict_[ch] = cur_index
+                    self.dict_[cur_index] = ch
+
+                sent_vec.append(self.inv_dict_[ch])
+
+            return sent_vec
+
+        for sent_pair in pair_corpus:
+            first = vectorize_update_dict(sent_pair[0])
+            second = vectorize_update_dict(sent_pair[1])
+
+            self.corpus_.append((first, second))
+
+    def generator(self, batch_size=128):
+        dict_size = len(self.dict_)
+        EOS_ONEHOT = np_utils.to_categorical([self.inv_dict_[self._EOS_]], dict_size)
+
+        data_x = []
+        data_y = []
+
+        i = 0
+        corpus_length = len(self.corpus_)
+        while True:
+            sent_x, sent_y = self.corpus_[i % corpus_length]
+            data_x.append(np_utils.to_categorical(sent_x, dict_size))
+            data_y.append(np_utils.to_categorical(sent_y, dict_size))
+
+            if (i + 1) % batch_size == 0:
+                yield (
+                    pad_sequences(data_x, self.max_len, value=EOS_ONEHOT),
+                    pad_sequences(data_y, self.max_len, value=EOS_ONEHOT),
+                )
+                data_x = []
+                data_y = []
+
+            i += 1
